437e11b5f19415d90f0aabded52f0ecae9700bc3
Tobias van der Werff <33268192+tobiasvanderwerff@users.noreply.github.com>
2022-05-25 14:55:53 +0200

    Fix wrong attribute reference for SAR

--- thesis/writer_code/lit_models.py
+++ thesis/writer_code/lit_models.py
@@ -372,7 +372,7 @@ class LitWriterCodeAdaptiveModel(LitBaseEpisodic):
                 label_encoder=label_encoder,
                 params_to_log=model_params_to_log,
             )
-            feature_size = base_model.rnn_encoder.input_size
+            feature_size = base_model.model.lstm_encoder.rnn_encoder.input_size
 
         # Initialize meta-model.
         if main_model_arch == "WriterCodeAdaptiveModel":
